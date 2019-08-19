969c1f3ea85fa7e5d841805900a6e3404c43e950
infosalim <salimh5757@gmail.com>
2019-08-20 01:29:43 +0600

    project setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+// import styles
+import './homepage.styles.scss';
+
+// components
+import HomePage from './hpmepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
