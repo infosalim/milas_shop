2163eaa124615ba46f9d37564a8ac8ccb4b3d0e7
infosalim <salimh5757@gmail.com>
2019-08-22 00:22:27 +0600

    created collectio preview component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import { Switch ,Route } from 'react-router-dom';
 import './App.css';
 // components
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
-  const Hats = () =>(
-    <div>Hello</div>
-  )
   return (
     <Switch>
       <Route exact component={HomePage} path='/' />
-      <Route exact component={Hats} path='/hats' />
+      <Route exact component={ShopPage} path='/shop' />
     </Switch>
   );
 }
