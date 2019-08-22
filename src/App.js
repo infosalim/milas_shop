d553f525a03f80262e94e1fd7413de7ac4063dbc
infosalim <salimh5757@gmail.com>
2019-08-23 00:28:56 +0600

    created header and navigate to other pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Switch ,Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 // components
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-    <Switch>
-      <Route exact component={HomePage} path='/' />
-      <Route exact component={ShopPage} path='/shop' />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact component={HomePage} path='/' />
+        <Route exact component={ShopPage} path='/shop' />
+      </Switch>
+    </div>
   );
 }
 
