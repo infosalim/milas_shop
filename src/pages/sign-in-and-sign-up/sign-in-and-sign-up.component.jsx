47db92a778b08ae4edc639a4c4dd4528d17f20b4
infosalim <salimh5757@gmail.com>
2019-08-25 23:27:13 +0600

    created sign up component and sign up functionality with firebase database

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,10 +3,12 @@ import './sign-in-and-sign-up.styles.scss';
 
 // component
 import SingIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
     <div className="sign-in-and-sign-up">
         <SingIn/>
+        <SignUp/>
     </div>
 );
 
