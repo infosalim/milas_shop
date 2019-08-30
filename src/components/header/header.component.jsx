9c6537656a47d594e0341f2872e9fe60db8a2ff5
infosalim <salimh5757@gmail.com>
2019-08-30 12:32:40 +0600

    triggered cart dropdown via redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,11 +3,14 @@ import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/logo.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import './header.styles.scss';
 
 import { auth } from "../../firebase/firebase.utils";
 
-const Header =  ({ currentUser }) => (
+const Header =  ({ currentUser, hidden }) => (
     <div className="header">
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -21,12 +24,17 @@ const Header =  ({ currentUser }) => (
                 :
                 <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon/>
         </div>
+        {
+            hidden ? null : <CartDropdown/>
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{ currentUser }, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
